8600dce0dc8be853a9656000dffc2a3d77120ee1
prasidhinandwani <nandwaniprasidhi@gmail.com>
2026-06-12 23:55:08 +0530

    Create SearchInsert.java

---
+++ SearchInsert.java
@@ -0,0 +1,18 @@
+class SearchInsert {
+    public int searchInsert(int[] a, int t) {
+        int l = 0, r = a.length - 1;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+
+            if (a[m] == t)
+                return m;
+            else if (a[m] < t)
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+
+        return l;
+    }
+}
